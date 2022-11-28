2822bb760b980eb2649c2845be8cd6a1533927f3
NerosJourney <sandman4264@gmail.com>
2022-11-28 12:10:34 -0500

    Switched to express routing

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-const http = require('http')
+const path = require('path');
+const express = require('express')
 
-const host = 'localhost'
-const port = 8000
+const app = express()
+const port = 3000
 
-const requestListener = function (req, res) {
-    res.writeHead(200);
-    res.end("My first server!");
-};
+let userLoggedIn = false;
 
-const server = http.createServer(requestListener);
-server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
-});
+app.get('/', (req, res) => {
+    if(userLoggedIn)
+        res.sendFile(path.join(__dirname, '/pages/home.html'))
+    else
+        res.redirect('/login')
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'pages/login.html'))
+    userLoggedIn = true;
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
