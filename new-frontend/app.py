f18254aa8503074aeba43b2cab8b8d79cedb0c0c
NerosJourney <sandman4264@gmail.com>
2022-12-02 18:30:10 -0500

    a bunch of stuff

--- new-frontend/app.py
+++ new-frontend/app.py
@@ -10,11 +10,6 @@ def login():
 def register():
     return render_template('register.html')
 
-@app.route("/")
-@app.route("/home")
-def home():
-    return render_template('home.html')
-
 @app.route("/account/orders")
 def orders():
     return render_template('order_history.html')
@@ -23,6 +18,7 @@ def orders():
 def settings():
     return render_template('account_settings.html', data={"name":"Bailey Wimer"})
 
+@app.route("/")
 @app.route("/catalog")
 def catalog():
     return render_template('catalog.html', items=[{"name": "Test", "price": 300}, {"name": "Kent Shirt", "price": 27.99}, {"name": "Hat", "price": 10.95}, {"name": "Bill Reed", "price": 100000.00}])
@@ -33,7 +29,7 @@ def item():
 
 @app.route("/cart")
 def cart():
-    return render_template('cart.html')
+    return render_template('cart.html', cart=[1, 2, 3, 4, 5])
 
 if __name__ == '__main__':
     app.run()
