96c0b53c4997b6f069eefb08809fb3c3ab84a759
NerosJourney <sandman4264@gmail.com>
2022-12-02 20:07:29 -0500

    UI working

--- new-frontend/app.py
+++ new-frontend/app.py
@@ -31,12 +31,14 @@ def item():
 
 @app.route("/cart")
 def cart():
-    r = requests.get("http://localhost:3000/cart/info", data={"customer": 1})
-    cart_info = json.loads(r.text)
-    cart_info['price'] = round(cart_info['price'], 2)
-    cart_info['weight'] = round(cart_info['weight'], 2)
-    r = requests.get("http://localhost:3000/cart/list", data={"customer": 1})
-    cart = json.loads(r.text)
+    # r = requests.get("http://localhost:3000/cart/info", data={"customer": 1})
+    # cart_info = json.loads(r.text)
+    # cart_info['price'] = round(cart_info['price'], 2)
+    # cart_info['weight'] = round(cart_info['weight'], 2)
+    # r = requests.get("http://localhost:3000/cart/list", data={"customer": 1})
+    # cart = json.loads(r.text)["item"]
+    cart = []
+    cart_info = {}
     return render_template('cart.html', cart=cart, cart_info=cart_info)
 
 @app.route("/catalog/item/image")
