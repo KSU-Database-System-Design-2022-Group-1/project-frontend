2ccc2d09375dbe148d83c70408b32855e2a9c017
NerosJourney <sandman4264@gmail.com>
2022-12-04 01:03:44 -0500

    stuff

--- new-frontend/app.py
+++ new-frontend/app.py
@@ -4,13 +4,16 @@ import json
 
 app = Flask(__name__)
 
+
 @app.route("/login")
 def login():
     return render_template('login.html')
 
+
 @app.route("/register")
 def register():
     return render_template('register.html')
+
 
 @app.route("/account/orders")
 def orders():
@@ -18,18 +21,22 @@ def orders():
     orders = json.loads(r.text)["orders"]
     return render_template('order_history.html', orders=orders)
 
+
 @app.route("/account/settings")
 def settings():
-    return render_template('account_settings.html', data={"name":"Bailey Wimer"})
+    return render_template('account_settings.html', customer={"first_name": "Bailey", "last_name": "Wimer", "email": "bwimer3@kent.edu", "shipping_address": "123 Kent Rd. NE", "shipping_city": "Kent", "shipping_zip": "12345", "billing_address": "123 Kent Rd. NE", "billing_city": "Kent", "billing_zip": "12345"})
+
 
 @app.route("/")
 @app.route("/catalog")
 def catalog():
     return render_template('catalog.html', items=[{"name": "Test", "price": 300}, {"name": "Kent Shirt", "price": 27.99}, {"name": "Hat", "price": 10.95}, {"name": "Bill Reed", "price": 100000.00}])
 
+
 @app.route("/catalog/item")
 def item():
-    return render_template('item.html')
+    return render_template('item.html', item={"name": "Test", "description": "This is a test and only a test", "category": "Shirt", "price": 52.95, "stock": 22, "variants": [{"size": "M", "color": "Blue"}, {"size": "L", "color": "Blue"}, {"size": "XL", "color": "Blue"}, {"size": "XL", "color": "Red"}]})
+
 
 @app.route("/cart")
 def cart():
@@ -40,9 +47,11 @@ def cart():
     cart = json.loads(r.text)["items"]
     return render_template('cart.html', cart=cart, cart_info=cart_info)
 
+
 @app.route("/catalog/item/image")
 def image():
     return
 
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
